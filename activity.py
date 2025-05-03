315cb4b2f3e8d7e2abcb34970448f2cce78f3cc0
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-02 23:50:45 -0300

    fix: adjusting the activity

--- activity.py
+++ activity.py
@@ -1,6 +1,6 @@
-numero_um = input("Entre com um número: ")
-numero_dois = input("Entre com outro número: ")
-print(f"A soma dos números é: {int(numero_um) + int(numero_dois)}")
-print(f"A subtração dos números é: {int(numero_um) - int(numero_dois)}")
-print(f"A multiplicação dos números é: {int(numero_um) * int(numero_dois)}")
+numero_um = int(input("Entre com um número: "))
+numero_dois = int(input("Entre com outro número: "))
+print(f"A soma dos números é: {numero_um + numero_dois}")
+print(f"A subtração dos números é: {numero_um - numero_dois}")
+print(f"A multiplicação dos números é: {numero_um * numero_dois}")
 
