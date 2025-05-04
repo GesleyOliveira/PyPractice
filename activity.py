dbd987d39d86b16ee79634bee587ca3936ec4d07
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-03 23:52:48 -0300

    Style: adding comments

--- activity.py
+++ activity.py
@@ -4,3 +4,14 @@ print(f"A soma dos números é: {numero_um + numero_dois}")
 print(f"A subtração dos números é: {numero_um - numero_dois}")
 print(f"A multiplicação dos números é: {numero_um * numero_dois}")
 
+# Comentário em uma linha
+"""Comentário em múltiplas linhas
+    Comentário em múltiplas linhas
+    Comentário em múltiplas linhas
+"""
+# Ou
+'''
+Comentário em múltiplas linhas  
+Comentário em múltiplas linhas
+Comentário em múltiplas linhas
+'''
